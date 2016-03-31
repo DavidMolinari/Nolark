763a6af82fa5f0da6d3d0391c2cf65021e77ef2b
Sevenn <thomas.s833@gmail.com>
2016-03-31 16:01:16 +0200

    Passage sur un keyup plutôt qu'un click

--- js/simulation.js
+++ js/simulation.js
@@ -1,54 +1,69 @@
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
 
-    window.document.querySelector("#btn_calculer").addEventListener("click", function() {
+    var cellules = document.querySelectorAll(".simulationForm");
 
-      const salaireFix = 1100.0;
 
-      var S20Vendus = parseInt(window.document.querySelector("#S20").value);
-      var XSpiritVendus = parseInt(window.document.querySelector("#XSpirit").value);
-      var MultitecVendus = parseInt(window.document.querySelector("#Multitec").value);
-      var ancienneteNbr = parseInt(window.document.querySelector("#anciennete").value);
+    for (var i = 0; i < cellules.length; i++) {
+        cellules[i].addEventListener("keyup", function () {
 
-      var total = salaireFix + comissionx5(XSpiritVendus) +
-comissionS20(S20Vendus) + comissionMulti(MultitecVendus) + aciennete(ancienneteNbr, salaireFix);
+            const salaireFix = 1100.0;
 
-alert("la remuneration totale est : " + total + "€");
-}, false);
+            var S20Vendus = parseInt(window.document.querySelector("#S20").value);
+            var XSpiritVendus = parseInt(window.document.querySelector("#XSpirit").value);
+            var MultitecVendus = parseInt(window.document.querySelector("#Multitec").value);
+            var ancienneteNbr = parseInt(window.document.querySelector("#anciennete").value);
+
+            var total = salaireFix + comissionx5(XSpiritVendus) +
+                    comissionS20(S20Vendus) + comissionMulti(MultitecVendus) + aciennete(ancienneteNbr, salaireFix);
+
+            console.log(total);
+            document.getElementById("result_total").value = total;
+
+        }, false);
+    }
 
 
 // Fonction Calcul de comission des casques X5, en parametres le nombre de casques vendus
-function comissionx5(nb){
-  if(nb<50) return 0;
-  else {
-    return 350.0*0.06*nb;
-}}
+    function comissionx5(nb) {
+        if (nb < 50)
+            return 0;
+        else {
+            return 350.0 * 0.06 * nb;
+        }
+    }
 
 // Fonction Calcul de comission des casques S20, en parametres le nombre de casques vendus
-function comissionS20(nb){
-  return nb*140.0*0.02;
-}
+    function comissionS20(nb) {
+        return nb * 140.0 * 0.02;
+    }
 
 // Fonction Calcul de comission des casques Multitec, en parametres le nombre de casques vendus
-function comissionMulti(nb){
-  if(nb <= 20) {
-    return nb*180.0*0.04;}
-  else if (nb <= 50)
-  {
-    return ((20 * 180.0*0.04) + ((nb - 20) * 180.00*0.06));
+    function comissionMulti(nb) {
+        if (nb <= 20) {
+            return nb * 180.0 * 0.04;
+        }
+        else if (nb <= 50)
+        {
+            return ((20 * 180.0 * 0.04) + ((nb - 20) * 180.00 * 0.06));
+        }
+        else
+        {
+            return (20 * 180.0 * 0.04) + (30 * 180.00 * 0.06) + ((nb - 50) * 180.0 * 0.1);
+        }
     }
-  else
-  { return (20*180.0*0.04) + (30*180.00*0.06) + ((nb - 50) * 180.0*0.1);
-}
-}
 // Fonction Calcul de comission d'ancienneté  : Le nombre d'année et le salaire fixe en param
-function aciennete(nbAnnee, salaireFix){
-  if (nbAnnee <= 5)
-  { return 0.0; }
-  else if (nbAnnee > 5 && nbAnnee <= 10)
-  { return salaireFix * 0.03;
-    }
-  else return salaireFix * 0.06;
+    function aciennete(nbAnnee, salaireFix) {
+        if (nbAnnee <= 5)
+        {
+            return 0.0;
+        }
+        else if (nbAnnee > 5 && nbAnnee <= 10)
+        {
+            return salaireFix * 0.03;
+        }
+        else
+            return salaireFix * 0.06;
 
-}
+    }
 }, false);
